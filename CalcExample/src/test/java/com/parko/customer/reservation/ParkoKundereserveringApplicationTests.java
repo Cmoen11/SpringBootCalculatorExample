83d1806acfb07a07c5e6fb1d139f003cec27f3b4
Christian Moen <cmoen11@gmail.com>
2017-03-24 20:24:34 +0100

    calculator

---
+++ CalcExample/src/test/java/com/parko/customer/reservation/ParkoKundereserveringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.parko.customer.reservation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkoKundereserveringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
